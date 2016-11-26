2a98004726f532b1e583fb5fb660593e62253d5c
Donovan De Smedt <donovandesmedt@live.be>
2016-11-26 15:00:35 +0100

    Initial Commit

---
+++ DBVisualizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBVisualizer
+//
+//  Created by Donovan De Smedt on 26/11/16.
+//  Copyright © 2016 Donovan De Smedt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
